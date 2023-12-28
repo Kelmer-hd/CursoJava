348a4441e45a6243360918192023db933bf074a8
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 14:44:16 -0500

    Constantes y operadores

---
+++ ConstantesYOperadores/src/Clase/Main.java
@@ -0,0 +1,32 @@
+package Clase;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            * Arimeticos:
+              + -> suma
+              - -> resta
+              * -> multiplicación
+              / -> división
+
+             * Logicos
+               > mayor que
+               < menor que
+               <> menor o mayor que
+               != distinto que
+               == igual que
+               && y logico
+               !! o logico
+
+             * Incremento y Decremento
+                ++ incremento
+                -- decremento
+                += n : incremento
+                -= n : decremento
+
+              * Concatenación:
+                 + -> une o concatena
+
+        */
+    }
+}
