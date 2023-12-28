48c0c6e1e06d6cab33c91af846c65d4dc3e19c19
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 14:25:29 -0500

    Tipos de datos en java, declaración de variables e inicialización, ejemplos de varibales e inicizalición de variables

---
+++ TiposDatos/src/Ejercicios/Variables.java
@@ -0,0 +1,16 @@
+package Ejercicios;
+public class Variables {
+    public static void main(String[] args) {
+        // Variables
+
+        int edad; // declarar la variable
+        edad = 35; // iciciarlizar la variable
+        System.out.println("La edad es: " +edad);
+
+        edad = 70; // cambiar el valor de la variable
+        System.out.println("Ahora la edad es: " +edad);
+
+
+    }
+
+}
