4a3c5a99ed92472f903cebb69d6ad63c50ed30d0
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 14:59:56 -0500

    Constantes y operadores

--- ConstantesYOperadores/src/Practicas/Ejercicios.java
+++ ConstantesYOperadores/src/Practicas/Ejercicios.java
@@ -1,4 +1,21 @@
 package Practicas;
 public class Ejercicios {
+    public static void main(String[] args) {
+        int a = 5;
+        int b;
+        b = 7;
 
+        int c = b + a;
+        System.out.println("La suma es: " + c);
+
+        // Inclentamos la suma en 1
+
+        c++;
+        System.out.println("Ahora es: " + c);
+
+        // Incrementamos en 6:
+
+        c += 6;
+        System.out.println("Ahora la suma es: " + c);
+    }
 }
