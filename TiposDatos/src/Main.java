f790cabf0db4c13f11edfc47e677ebcb6cc178a0
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 14:01:14 -0500

    Tipos de datos en java

---
+++ TiposDatos/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Enteros:
+            - int
+            - short
+            - long
+            - byte
+
+           Decimales:
+            - float
+            - Double
+
+           Char
+
+        */
+    }
+}
