65b007ba1f957947be57521af876b4e9a4eed0ea
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-28 15:06:52 -0500

    Ejercicios para practicar

---
+++ ConstantesYOperadores/src/Practicas/Ejercicios2.java
@@ -0,0 +1,13 @@
+package Practicas;
+
+public class Ejercicios2 {
+    public static void main(String[] args) {
+        final int c = 5; // Declaramos una constante
+        System.out.println(c);
+
+        final double apulgadas = 2.54;
+        double cm = 6;
+        double resultado = cm / apulgadas;
+        System.out.println("En "+ cm + " cm hay "+ resultado+ " pulgadas");
+    }
+}
