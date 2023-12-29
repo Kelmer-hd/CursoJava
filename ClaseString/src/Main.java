af5ee924fb981913a76eac320f5bf10fd460afae
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2023-12-29 10:34:09 -0500

    Clase String

--- ClaseString/src/Main.java
+++ ClaseString/src/Main.java
@@ -11,7 +11,19 @@ public class Main {
 
         int ultima_letra;
         ultima_letra = nombre.length();
-        System.out.println("La ultima letra de mi nombre es"+ nombre.charAt(ultima_letra - 1));
+        System.out.println("La ultima letra de mi nombre es "+ nombre.charAt(ultima_letra - 1));
+
+        // More examples
+        String frese = "Hoy es un gran dia para aprender a programar en Java";
+        String frase_resument = frese.substring(0,29) + " irnos a la playa y olvidarnos de todo...";
+        System.out.println("" +frase_resument);
+
+        // More examples
+        String alumno1, alumno2;
+        alumno1 = "David";
+        alumno2 = "david";
+        System.out.println(alumno1.equalsIgnoreCase(alumno2));
+
 
     }
 }
