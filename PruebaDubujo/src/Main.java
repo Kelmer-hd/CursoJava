b6ecf40db88ca4be2ae43aedb14c9e85b058727a
kelmerhd <50677633+Kelmer-hd@users.noreply.github.com>
2024-01-22 20:36:05 -0500

    Prubas de Dibujo

--- PruebaDubujo/src/Main.java
+++ PruebaDubujo/src/Main.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,7 +28,29 @@ class LaminaConFiguras extends JPanel{
 
         //g.drawRect(50, 50, 200, 200);
         //g.drawLine(100, 100, 300, 200);
+        //g.drawArc(50, 100,100, 200, 120,150);
 
-        g.drawArc(50, 100,100, 200, 120,150);
+        Graphics2D g2 = (Graphics2D) g;
+        Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150);
+        g2.draw(rectangulo);
+
+        Ellipse2D elipse = new Ellipse2D.Double();
+        elipse.setFrame(rectangulo);
+        g2.draw(elipse);
+
+        g2.draw(new Line2D.Double(100, 100, 300, 250));
+
+        double centroenX = rectangulo.getCenterX();
+        double centroenY = rectangulo.getCenterY();
+
+        double radio = 150;
+
+        Ellipse2D circulo = new Ellipse2D.Double();
+        circulo.setFrameFromCenter(centroenX, centroenY, centroenX + radio, centroenY + radio);
+
+        g2.draw(circulo);
+
+
+
     }
 }
